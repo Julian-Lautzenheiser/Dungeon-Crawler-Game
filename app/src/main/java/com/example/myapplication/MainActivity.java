f5c0d2b01ac549d9ea70757f4b446ea5871b8250
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-09-22 13:19:16 -0400

    Added welcome screen with appropriate functionality

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.RadioGroup;
@@ -14,48 +15,18 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button startBtn = findViewById(R.id.startButton);
-        TextView nameBox = findViewById(R.id.textName);
+        Button startBtn = findViewById(R.id.startBtn);
+        Button closeButton = findViewById(R.id.closeBtn);
+
+        closeButton.setOnClickListener(v -> {
+            finish();
+        });
 
         //Check if name is valid, if so then set difficulty based on radioButton
         startBtn.setOnClickListener(v -> {
-            String name = (String) nameBox.getText();
-            if (name == null || name.trim() == "") {
-                finish(); //Leave current listener if name is not valid
-            } else {
-                RadioGroup difficultyRadioGroup = findViewById(R.id.difficultyRadioGroup);
-                double difficulty = 1;
-                int radioId = difficultyRadioGroup.getCheckedRadioButtonId();
-                if (radioId == R.id.radioEasy) {
-                    difficulty = 0.5;
-                } else if (radioId == R.id.radioMedium) {
-                    difficulty = 0.75;
-                } else if (radioId == R.id.radioHard) {
-                    difficulty = 1;
-                } else {
-                    difficulty = 0.5;
-                }
-
-                RadioGroup spriteRadioGroup = findViewById(R.id.spriteRadioGroup);
-                int sprite = 1;
-                int spriteId = spriteRadioGroup.getCheckedRadioButtonId();
-
-                if (spriteId == R.id.radioOne) { //Puts the sprite as an extra
-                    sprite = 1;
-                } else if (spriteId == R.id.radioTwo) {
-                    sprite = 2;
-                } else if (spriteId == R.id.radioThree) {
-                    sprite = 3;
-                } else {
-                    sprite = 1;
-                }
-
-                Intent game = new Intent(MainActivity.this, GameActivity.class);
-                game.putExtra("difficulty", difficulty);
-                game.putExtra("sprite", sprite);
-                startActivity(game);
-                finish();
-            }
+            Intent config = new Intent(MainActivity.this, ConfigurationActivity.class);
+            startActivity(config);
+            finish();
         });
     }
 }

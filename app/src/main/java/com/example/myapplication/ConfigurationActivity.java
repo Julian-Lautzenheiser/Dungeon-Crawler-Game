cde98228ae4fe9be7dacdd75db28a68b3c62b7fd
Nawal Reza <nawalreza07@gmail.com>
2023-09-22 11:21:30 -0400

    Update of Configuration Screen

--- app/src/main/java/com/example/myapplication/ConfigurationActivity.java
+++ app/src/main/java/com/example/myapplication/ConfigurationActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
@@ -16,6 +20,7 @@ public class ConfigurationActivity extends AppCompatActivity {
         setContentView(R.layout.configuration_screen);
         Button startBtn = findViewById(R.id.StartGameButton);
         TextView nameBox = findViewById(R.id.UsernameText);
+        ImageView image = findViewById(R.id.spriteimage);
 
         //Check if name is valid, if so then set difficulty based on radioButton
         startBtn.setOnClickListener(v -> {
@@ -23,31 +28,35 @@ public class ConfigurationActivity extends AppCompatActivity {
             if (name == null || name.trim() == "") {
                 finish(); //Leave current listener if name is not valid
             } else {
-                RadioGroup difficultyRadioGroup = findViewById(R.id.difficultyRadioGroup);
+                RadioGroup difficultyRadioGroup = findViewById(R.id.difficultyGroup);
                 double difficulty = 1;
                 int radioId = difficultyRadioGroup.getCheckedRadioButtonId();
-                if (radioId == R.id.radioEasy) {
+                if (radioId == R.id.easyDifficulty) {
                     difficulty = 0.5;
-                } else if (radioId == R.id.radioMedium) {
+                } else if (radioId == R.id.mediumDifficulty) {
                     difficulty = 0.75;
-                } else if (radioId == R.id.radioHard) {
+                } else if (radioId == R.id.hardDifficulty) {
                     difficulty = 1;
                 } else {
                     difficulty = 0.5;
                 }
 
-                RadioGroup spriteRadioGroup = findViewById(R.id.spriteRadioGroup);
+                RadioGroup spriteRadioGroup = findViewById(R.id.CharacterGroup);
                 int sprite = 1;
                 int spriteId = spriteRadioGroup.getCheckedRadioButtonId();
 
-                if (spriteId == R.id.radioOne) { //Puts the sprite as an extra
+                if (spriteId == R.id.Sprite1) { //Puts the sprite as an extra
                     sprite = 1;
-                } else if (spriteId == R.id.radioTwo) {
+                    image.setImageDrawable(getResources().getDrawable(R.drawable.sword_warrior));
+                } else if (spriteId == R.id.Sprite2) {
                     sprite = 2;
-                } else if (spriteId == R.id.radioThree) {
+                    image.setImageDrawable(getResources().getDrawable(R.drawable.wizard_warrior));
+                } else if (spriteId == R.id.Sprite3) {
                     sprite = 3;
+                    image.setImageDrawable(getResources().getDrawable(R.drawable.fighter_warrior));
                 } else {
                     sprite = 1;
+                    image.setImageDrawable(getResources().getDrawable(R.drawable.sword_warrior));
                 }
 
                 Intent game = new Intent(ConfigurationActivity.this, GameActivity.class);

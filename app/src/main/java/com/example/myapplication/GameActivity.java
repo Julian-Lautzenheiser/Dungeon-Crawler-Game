c16e98fdd06dd20f7733147f7789799e2b1b19a4
Julian Lautzenheiser <julian.lautzenheiser@gmail.com>
2023-09-23 14:45:24 -0400

    Add sprite and health and name attributes to game screen

--- app/src/main/java/com/example/myapplication/GameActivity.java
+++ app/src/main/java/com/example/myapplication/GameActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,24 +16,70 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 public class GameActivity extends AppCompatActivity {
 
 
-    private PlayerView playerView;
-    private float playerX, playerY;
-    ConstraintLayout gameLayout;
+    private ImageView playerView;
+    private TextView nameText;
+    private TextView healthText;
 
+    private float playerX, playerY;
     private double difficulty;
-    private String name;
+    ConstraintLayout gameLayout;
+    int screenWidth;
+    int screenHeight;
+    final int PLAYER_TEXT_OFFSET = 100;
+    final int HEALTH_BASE = 100;
+    final int MAX_SIZE = 300;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         gameLayout = findViewById(R.id.gameLayout);
-
         Button exitButton = findViewById(R.id.buttonExit);
+        screenWidth = getResources().getDisplayMetrics().widthPixels;
+        screenHeight = getResources().getDisplayMetrics().heightPixels;
+        // Spawn player in middle of screen
+        playerX = screenWidth / 2;
+        playerY = screenHeight / 2;
+
+        int sprite = getIntent().getIntExtra("sprite", 1);
+        playerView = new ImageView(this);
+        if (sprite == 1) { //Sets the sprite based on input
+            playerView.setImageResource(R.drawable.sword_warrior); //Warrior - Sword
+        } else if (sprite == 2) {
+            playerView.setImageResource(R.drawable.wizard_warrior); //Mage - Wizard
+        } else if (sprite == 3) {
+            playerView.setImageResource(R.drawable.fighter_warrior); //Fighter - Fighter
+        }
+        playerView.setAdjustViewBounds(true); //Allows for resizing of sprite while keeping aspect ratio
+        playerX /= 2; //TEMP
+        playerY -= 300; //MOVES PLAYER CHARACTER INTO WHITESPACE
+        playerView.setX(playerX);
+        playerView.setY(playerY);
+        playerView.setMaxHeight(MAX_SIZE); //Makes the sprite not crazy big
+        playerView.setMaxWidth(MAX_SIZE);
+
+        gameLayout.addView(playerView);
+
+        nameText = new TextView(this);
+        nameText.setText(getIntent().getStringExtra("name"));
+        nameText.setX(playerX);
+        nameText.setY(playerY - PLAYER_TEXT_OFFSET);
+        gameLayout.addView(nameText); //Creates name and attaches it to character
+
+        difficulty = getIntent().getDoubleExtra("difficulty", 0.5);
+        healthText = new TextView(this);
+        healthText.setText(Double.toString((HEALTH_BASE / difficulty))); //Dividing health by decimal increases health with lower difficulty
+        healthText.setX(playerX);
+        healthText.setY(playerY + playerView.getMaxHeight());
+        gameLayout.addView(healthText); //Creates health text and attaches it to character
+
+
 
         exitButton.setOnClickListener(v -> {
-            //Intent end = new Intent(GameActivity.this, finaliActivity.class);
-            //startActivity(end);
+            Intent end = new Intent(GameActivity.this, GameEndActivity.class);
+            startActivity(end);
             finish();
         });
 

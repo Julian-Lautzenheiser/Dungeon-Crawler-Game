bc258c9c8b13ddb5a9fc3d64957a527fba8f5811
jpise3 <63403110+jaipise@users.noreply.github.com>
2023-11-03 11:59:15 -0400

    Created enemy with factory pattern and implemented 4 different types:

---
+++ core/src/main/java/com/example/myapplication/Models/DemonEnemy.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.Models;
+
+public class DemonEnemy implements Enemy {
+    // Implement attributes and behaviors for the Demon
+    // e.g., sprite, speed, size
+    @Override
+    public void move() {
+        // Implement movement logic
+    }
+
+    @Override
+    public void attack() {
+        // Implement attack logic
+    }
+
+    @Override
+    public void display() {
+        // Implement display logic
+    }
+}
---
+++ core/src/main/java/com/example/myapplication/ViewModels/EnemyFactory.java
@@ -0,0 +1,23 @@
+package com.example.myapplication.ViewModels;
+
+import com.example.myapplication.Models.DemonEnemy;
+import com.example.myapplication.Models.Enemy;
+import com.example.myapplication.Models.GoblinEnemy;
+import com.example.myapplication.Models.OgreEnemy;
+import com.example.myapplication.Models.SkeletonEnemy;
+
+public class EnemyFactory {
+    public Enemy createEnemy(String enemyType) {
+        if (enemyType.equalsIgnoreCase("Ogre")) {
+            return new OgreEnemy();
+        } else if (enemyType.equalsIgnoreCase("Skeleton")) {
+            return new SkeletonEnemy();
+        } else if (enemyType.equalsIgnoreCase("Goblin")) {
+            return new GoblinEnemy();
+        } else if (enemyType.equalsIgnoreCase("Demon")) {
+            return new DemonEnemy();
+        }
+
+        return null; // Return null for unsupported enemy types
+    }
+}

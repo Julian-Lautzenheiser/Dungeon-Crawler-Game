c01efaa3eac2777b9f8046a8d9c8a94806fd7d46
Julian Lautzenheiser <julian.lautzenheiser@gmail.com>
2023-11-25 16:09:42 -0600

    Create view model for attacking logic

---
+++ core/src/main/java/com/example/myapplication/ViewModels/AttackingViewModel.java
@@ -0,0 +1,41 @@
+package com.example.myapplication.ViewModels;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.example.myapplication.Models.Enemy;
+import com.example.myapplication.Models.Player;
+import com.badlogic.gdx.math.Rectangle;
+
+import java.util.List;
+
+public class AttackingViewModel {
+private Player player = Player.getInstance();
+
+    public void checkAttack(List<Enemy> enemyList) {
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)
+                || Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            Gdx.input.setOnscreenKeyboardVisible(false);
+            player.setAttacking(true);
+            attack(enemyList);
+        } else {
+            player.setAttacking(false);
+        }
+    }
+
+    private void attack(List<Enemy> enemyList) {
+        Rectangle enemy = new Rectangle();
+        Rectangle weapon = new Rectangle();
+
+        float weaponX = player.getPlayerX() + player.getWidth() / 2;
+        float weaponY = player.getPlayerY() + player.getHeight() / 4;
+        weapon.set(weaponX, weaponY, 32, 16);
+
+        for (Enemy e: enemyList) {
+            enemy.set(e.getPositionX(), e.getPositionY(), e.getWidth(), e.getHeight());
+            if (enemy.overlaps(weapon)) {
+                e.damageTaken();
+            }
+        }
+    }
+
+}
